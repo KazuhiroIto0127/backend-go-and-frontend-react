ee0ce81b8e42f0ed40d90497688dd72ed0f6aba5
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2024-02-27 19:21:30 +0900

    tailwindcssが動作することを確認した

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div>
+        <button className="rounded-sm bg-blue-300" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
